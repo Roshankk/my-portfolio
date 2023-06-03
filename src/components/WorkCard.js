92ca1a8b374b92ed7e51cb5b611385ea57bea7ec
Roshan kalamkar <roshankalamkar801@gmail.com>
2023-06-04 01:36:36 +0530

    my-portfolio 04-06-2023

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,19 +1,49 @@
+// import './WorkCard.css'
+// import React from "react"
+// import { NavLink } from 'react-router-dom'
+// const WorkCard = (props) => {
+//     return (
+//         <div className="project-card">
+//         <img src={props.imgsrc} alt="image" />
+//         <h2 className="project-title">{props.title}</h2>
+//         <div className="pro-details">
+//             <p> {props.text}</p>
+//             <div className="pro-btns">
+//                 <NavLink to={props.view} className="btn"> View</NavLink>
+//                 <NavLink to={props.source} className="btn"> source</NavLink>
+//             </div>
+//         </div>
+//     </div>
+//     )
+// }
+
+// export default WorkCard;
+
 import './WorkCard.css'
 import React from "react"
 import { NavLink } from 'react-router-dom'
+
 const WorkCard = (props) => {
+    const handleViewClick = () => {
+        window.open(props.view, "_blank");
+    }
+
+    const handleSourceClick = () => {
+        window.open(props.source, "_blank");
+    }
+
     return (
         <div className="project-card">
-        <img src={props.imgsrc} alt="image" />
-        <h2 className="project-title">{props.title}</h2>
-        <div className="pro-details">
-            <p> {props.text}</p>
-            <div className="pro-btns">
-                <NavLink to={props.view} target="_blank" className="btn"> View</NavLink>
-                <NavLink to={props.source} className="btn"> source</NavLink>
+            <img src={props.imgsrc} alt="image" />
+            <h2 className="project-title">{props.title}</h2>
+            <div className="pro-details">
+                <p>{props.text}</p>
+                <div className="pro-btns">
+                    <button className="btn" onClick={handleViewClick}>View</button>
+                    <button className="btn" onClick={handleSourceClick}>Source</button>
+                </div>
             </div>
         </div>
-    </div>
     )
 }
 
